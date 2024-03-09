0bff9d7413a8c4c9f0091116d2cda8f63b84c58f
damiandrabek <damiandrabek1703@gmail.com>
2024-03-09 16:46:30 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  let chapterTitle = "I'm a made guy ";
+  let times = 4;
+
+</script>
+
+<header>
+  <h1>TYRANÁTOR SELEKTOR®</h1>
+</header>
+
+<main>
+
+  <h2>{(chapterTitle).repeat(times)}</h2>
+
+  <section class="controls">
+    <button on:click={() => times++}>JUST DO IT</button>
+  </section>
+  
+</main>
+
+<footer class="water"></footer>
