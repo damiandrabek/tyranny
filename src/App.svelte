999e46815d86a0c9b0b1d7450d43acfbff336aa1
damiandrabek <damiandrabek1703@gmail.com>
2024-03-12 16:29:43 +0100

    progress bar demonstrating animation of state change

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   // components
   import Wave from "./lib/Wave.svelte";
+  import { tweened } from "svelte/motion";
+  import { cubicOut, circOut } from "svelte/easing";
 
   // props
   export let books;
@@ -9,18 +11,26 @@
   let chapterTitle = "Do not obey in advance";
   let waving = true;
 
-  // functions
+  // randomise (choose random book)
   const chooseBook = () => {
     books = books.sort(() => 0.5 - Math.random());
-    console.log(chosenBook);
+    progress.set(Math.random());
   };
   $: chosenBook = books[0].title.toUpperCase() + ", " + books[0].page;
-  
+
+  // move chosenBook from unread to read books
   const readBook = () => {};
+
+  const progress = tweened(0, {
+    duration: 2000,
+    easing: circOut,
+  });
 </script>
 
 <header>
   <h1>TYRANNY SELECTOR®</h1>
+
+  <progress value={$progress} />
 
   <label for="waving">
     <input type="checkbox" id="waving" bind:checked={waving} />
@@ -29,27 +39,24 @@
 </header>
 
 <main>
-  <ul>
-    {#each books as book}
-      <li>
-        {JSON.stringify(book)}
-      </li>
-    {/each}
-  </ul>
-
-  <h2>{chapterTitle}</h2>
+  <p>{$progress}</p>
+  <h2>{chosenBook}</h2>
 
   <section class="controls">
     <img on:click={chooseBook} src="../public/gun.png" alt="cool gun" />
-    <img on:click={readBook} src="../public/anchor.png" alt="sweet ass anchor"/>
+    <img
+      on:click={readBook}
+      src="../public/anchor.png"
+      alt="sweet ass anchor"
+    />
   </section>
 </main>
 
 <footer class="water">
-  <Wave where="back" animation="big" waving={waving} />
-  <Wave where="middle" animation="tiny" waving={waving} />
+  <Wave where="back" animation="big" {waving} />
+  <Wave where="middle" animation="tiny" {waving} />
   <img class="twoMen" src="../public/twoMen.png" alt="two men" />
-  <Wave where="front" animation="small" waving={waving} />
+  <Wave where="front" animation="small" {waving} />
 </footer>
 
 <style>
@@ -58,5 +65,8 @@
     top: 1em;
     left: 1em;
     font-size: 1em;
+  }
+  progress {
+    width: 90%;
   }
 </style>
